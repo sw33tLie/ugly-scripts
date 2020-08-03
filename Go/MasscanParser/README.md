4f4484b7e84c7b3a32b5cefcf9937a11f50de1a2
sw33tLie <sw33tlie@protonmail.com>
2020-08-03 12:21:50 +0200

    Masscan Parser

---
+++ Go/MasscanParser/README.md
@@ -0,0 +1,15 @@
+# Masscan Parser
+This script can be used to parse Masscan's json output and print all IP:port combinations.
+
+Example:
+```
+$ sudo masscan 12.34.56.78/16 -p1-65535 --rate 100000 --output-format json --output-filename masscan-output.json
+```
+Then run this script:
+```
+$ go run main.go -f masscan-output.json
+12.34.0.14:80
+12.34.0.14:8443
+12.34.0.36:22
+...
+```
---
+++ Go/MasscanParser/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+
+	"github.com/tidwall/gjson"
+)
+
+func main() {
+	var filePath string
+	flag.StringVar(&filePath, "f", "", "example.json")
+	flag.Parse()
+
+	if filePath == "" {
+		flag.PrintDefaults()
+		log.Fatal("IP not set")
+	}
+
+	rawPortsFileBytes, _ := ioutil.ReadFile(filePath)
+	jsonPortsString := `{ "data":` + string(rawPortsFileBytes) + `}`
+	result := gjson.Get(jsonPortsString, "data")
+	portsMap := make(map[string][]int)
+
+	for _, name := range result.Array() {
+		jsonLine := name.String()
+		ip := gjson.Get(jsonLine, "ip").String()
+		port := gjson.Get(jsonLine, "ports.0.port").String()
+		portNumber, _ := strconv.Atoi(port)
+		portsMap[ip] = append(portsMap[ip], portNumber)
+	}
+
+	for ip, ports := range portsMap {
+		for _, port := range ports {
+			fmt.Println(ip + ":" + strconv.Itoa(port))
+		}
+	}
+}
